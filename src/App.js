d62adaed0be8c73e2fbdbb5c7d0fa51f55b7afdf
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-21 20:11:09 +0200

    Add basic implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from 'react';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+  const [ data, setData ] = React.useState('Not Found');
 
+  useEffect(() => {
+      console.log(data);
+  }, [data])
+ 
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text)
+          else setData('Not Found')
+        }}
+      />
+      <p>{data}</p>
+    </>
+  )
+}
+ 
 export default App;
